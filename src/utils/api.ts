74b4e218bccda8645108e5b9550e6073e1cf8fca
MaksymI <maksym.izmailov.lv@gmail.com>
2019-11-06 13:10:50 +0200

    correct  promise  usage

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,54 +12,44 @@ export const API_KEY_3 = '3f4ca4f3a9750da53450646ced312397';
 export const API_KEY_4 =
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjRjYTRmM2E5NzUwZGE1MzQ1MDY0NmNlZDMxMjM5NyIsInN1YiI6IjVhYzlmNWRkOTI1MTQxNjJhZTA1Njk0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fc4f9DVB6pFWh6hIjYe0NCC4pImdmNzDIfi_3Nb3tC4';
 
-export const callApi = (url = '', options = {}): Promise<{ results: Movie[] }> => {
-    return new Promise(resolve => {
-        fetch(`${API_URL}/${url}`, options)
-            .then(response => {
-                if (response.status < 400) {
-                    return response.json();
-                } else {
-                    throw response.json();
-                }
-            })
-            .then(data => {
-                resolve(data);
-            });
-    });
+export const callApi = async (url = '', options = {}): Promise<{ results: Movie[] }> => {
+    const response = await fetch(`${API_URL}/${url}`, options);
+    if (response.status < 400) {
+        return response.json();
+    } else {
+        throw response.json();
+    }
 };
 
-export const authenticate = (username: string, password: string): Promise<AuthenticateStatus> => {
-    return new Promise(resolve => {
-        fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-            .then(response => response.json())
-            .then(data => {
-                // const { request_token } = data;
-                // console.log('request_token', request_token);
-                fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
-                    method: 'POST',
-                    mode: 'cors',
-                    headers: {
-                        'content-type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        username,
-                        password,
-                        request_token: data.request_token || ''
-                    })
-                })
-                    .then(response => {
-                        console.log('response', response);
-                        // TODO: change to 400!
-                        if (response.status < 402) {
-                            return response.json();
-                        } else {
-                            throw response.json();
-                        }
-                    })
-                    .then((status: AuthenticateStatus) => {
-                        console.log('data', status);
-                        resolve(status);
-                    });
-            });
-    });
+export const authenticate = async (
+    username: string,
+    password: string
+): Promise<void | AuthenticateStatus> => {
+    const tokenResponse = await fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`);
+    const tokenResponseJson = await tokenResponse.json();
+    const { request_token } = tokenResponseJson;
+    console.log('request_token', request_token);
+    const authenticateStatusResponse = await fetch(
+        `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
+        {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                request_token
+            })
+        }
+    );
+
+    console.log('authenticateStatusResponse', authenticateStatusResponse);
+    // TODO: change to 400!
+    if (authenticateStatusResponse.status < 402) {
+        return authenticateStatusResponse.json();
+    } else {
+        throw authenticateStatusResponse.json();
+    }
 };
