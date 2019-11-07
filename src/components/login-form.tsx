eb0d4b422c18edd0f95a11e6b01dd17b152858d6
Maksym Izmailov <maksym.izmailov@cgm.com>
2019-11-07 16:08:27 +0200

    LoginForm on hooks

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,87 +1,69 @@
-import React, { Component, FormEvent, ChangeEvent, ReactElement } from 'react';
+import React, { FormEvent, ReactElement, useState, useEffect } from 'react';
 import { authenticate } from '../utils/api';
 
-const initialState: LoginFormState = {
-    username: '',
-    password: '',
-    error: null
-};
-type LoginFormState = {
-    [key: string]: string;
-};
-type LoginFormProps = { updateAuth: (value: boolean) => void };
+type LoginFormProps = { setIsAuth: (value: boolean) => void };
 
-export class LoginForm extends Component<LoginFormProps, LoginFormState> {
-    readonly state: LoginFormState = initialState;
-    readonly inputNameRef: React.RefObject<HTMLInputElement> = React.createRef();
+export function LoginForm(props: LoginFormProps): ReactElement<any> {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState<string | null>(null);
 
-    componentDidMount(): void {
-        this.inputNameRef.current.focus();
-    }
+    const inputNameRef: React.RefObject<HTMLInputElement> = React.createRef();
 
-    onChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
+    useEffect(() => {
+        inputNameRef.current.focus();
+    });
 
-    onSubmit = (event: FormEvent): void => {
+    const onSubmit = (event: FormEvent): void => {
         event.preventDefault();
-        console.log('onSubmit() this.state --', this.state);
 
-        authenticate(this.state.username, this.state.password)
+        authenticate(username, password)
             .then(data => {
-                this.props.updateAuth(true);
+                props.setIsAuth(true);
                 console.log('post data', data);
             })
             .catch(response => {
                 response.then((error: { status_message: string }) => {
                     console.log('error', error);
-                    this.setState({
-                        error: error.status_message
-                    });
+                    setError(error.status_message);
                 });
             });
     };
 
-    render(): ReactElement<any> {
-        return (
-            <div className="form-login-container">
-                <form className="form-login" onSubmit={this.onSubmit}>
-                    <h1 className="h3 mb-3 font-weight-normal text-center">Login</h1>
-                    <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="username"
-                            placeholder="username"
-                            name="username"
-                            value={this.state.username}
-                            ref={this.inputNameRef}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="password"
-                            placeholder="password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-lg btn-primary btn-block">
-                        Enter
-                    </button>
-                    {this.state.error ? (
-                        <div className="invalid-feedback">{this.state.error}</div>
-                    ) : null}
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="form-login-container">
+            <form className="form-login" onSubmit={onSubmit}>
+                <h1 className="h3 mb-3 font-weight-normal text-center">Login</h1>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="username"
+                        name="username"
+                        value={username}
+                        ref={inputNameRef}
+                        onChange={event => setUsername(event.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="password"
+                        name="password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                </div>
+                <button type="submit" className="btn btn-lg btn-primary btn-block">
+                    Enter
+                </button>
+                {error ? <div className="invalid-feedback">{error}</div> : null}
+            </form>
+        </div>
+    );
 }
